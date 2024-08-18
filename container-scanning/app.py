060ed1ba657f4c020e92576489bcf2b241bbfe39
GitiL <GitiL@skyvar.co.il>
2024-08-18 16:20:20 +0300

    try with rabbit

--- container-scanning/app.py
+++ container-scanning/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from waitress import serve
 # from azure.storage.queue import QueueClient, TextBase64EncodePolicy
 import pika
@@ -13,13 +13,15 @@ app = Flask(__name__)
 
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
+        response = request.get_json()
         credentials = pika.PlainCredentials("admin", "admin")
         connection = pika.BlockingConnection(pika.ConnectionParameters(host="172.171.129.95", credentials=credentials))
         channel = connection.channel()
+        massage = response["target"]["repository"]
         channel.queue_declare(queue="logs")
         channel.basic_publish(exchange='',
                               routing_key="logs",
-                              body="message")
+                              body=massage)
         connection.close() 
         return "success!"
 
