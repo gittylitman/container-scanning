e3aa11ce2397001076c9f1f2abc849ce39ac37cc
GitiL <GitiL@skyvar.co.il>
2024-08-18 16:09:58 +0300

    try with rabbit

--- container-scanning/app.py
+++ container-scanning/app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from waitress import serve
-from azure.storage.queue import QueueClient, TextBase64EncodePolicy
+# from azure.storage.queue import QueueClient, TextBase64EncodePolicy
 import pika
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import config.config_variables
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# import config.config_variables
 
 app = Flask(__name__)
  
 
-@app.route("/image_push", methods=["POST"])
+@app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
         credentials = pika.PlainCredentials("admin", "admin")
         connection = pika.BlockingConnection(pika.ConnectionParameters(host="172.171.129.95", credentials=credentials))
@@ -23,5 +23,5 @@ def send_to_image_scanning():
         connection.close() 
         return "success!"
 
-# if __name__ == "__main__":
-#     serve(app, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8080)
