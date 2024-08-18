bd78f336f6a43bf32193d1cfa95517dd3ece2741
GitiL <GitiL@skyvar.co.il>
2024-08-18 17:24:24 +0300

    try with rabbit

--- container-scanning/app.py
+++ container-scanning/app.py
@@ -1,12 +1,7 @@
 from flask import Flask, request
 from waitress import serve
-# from azure.storage.queue import QueueClient, TextBase64EncodePolicy
-import pika
-import os
-import sys
+from project.image_scanning import run_resource_graph_query
 
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-# import config.config_variables
 
 app = Flask(__name__)
  
@@ -14,16 +9,9 @@ app = Flask(__name__)
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
         response = request.get_json()
-        credentials = pika.PlainCredentials("admin", "admin")
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host="172.171.129.95", credentials=credentials))
-        channel = connection.channel()
-        massage = response["target"]["repository"]
-        channel.queue_declare(queue="logs")
-        channel.basic_publish(exchange='',
-                              routing_key="logs",
-                              body=massage)
-        connection.close() 
+        run_resource_graph_query(response["target"]["digest"],response["target"]["repository"])
         return "success!"
+
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8080)
---
+++ container-scanning/project/image_scanning.py
@@ -0,0 +1,49 @@
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.resourcegraph import ResourceGraphClient
+from azure.mgmt.resourcegraph.models import QueryRequest
+import pika
+
+
+def run_resource_graph_query(image_digest,image_name):
+        credential = DefaultAzureCredential()
+        client = ResourceGraphClient(credential)
+        query = set_resource_graph_query(image_digest, image_name)
+        result = client.resources(QueryRequest(query=query)).as_dict()
+        result=str(result)
+        if result=="":
+            result="hello!!!"
+        rabbit_massage=send_message_to_rabbitmq(result)
+        return rabbit_massage
+
+
+
+
+def set_resource_graph_query(image_digest,image_name):
+    query = f"""
+        securityresources
+        | where type =~ 'microsoft.security/assessments/subassessments'
+        | where properties.resourceDetails.ResourceProvider == 'acr'
+        | where properties contains '{image_digest}'
+        | summarize data = make_list(pack(
+            'CVE_ID', properties.id,
+            'Severity', properties.additionalData.vulnerabilityDetails.severity
+        )) by tostring(properties.resourceDetails.ResourceName)
+        | project ImageName='{image_name}' , Data=data
+    """
+    return query
+
+
+def send_message_to_rabbitmq(message):
+        credentials = pika.PlainCredentials("admin", "admin")
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host="172.171.129.95", credentials=credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue="logs")
+        channel.basic_publish(exchange='',
+                              routing_key="logs",
+                              body=message)
+        connection.close() 
+        return "success!"
+    
+
+
+    
