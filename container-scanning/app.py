83ff44d6dcda7b31ec5cc75506c4f6af5a2d6b2d
GitiL <GitiL@skyvar.co.il>
2024-08-21 11:46:10 +0300

    tryyyyyy

--- container-scanning/app.py
+++ container-scanning/app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 @app.route("/image_push_acr", methods=["POST"])
 def send_to_image_scanning():
         response = request.get_json()
-        run_resource_graph_query(response["target"]["digest"],response["target"]["repository"])
+        run_resource_graph_query("sha256:404a0f601f670d7670b4b2590da726fd52b482954b3163d6c13da11e98fa4964","newspace")
+        # run_resource_graph_query(response["target"]["digest"],response["target"]["repository"])
         return "success!"
 
 
